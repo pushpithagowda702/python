3b2781520b8c1d6b2e178c4e822e9d818f18f6e9
pushpitha <pushpithagowda702@gmail.com>
2023-06-17 18:39:55 +0530

    Anagram Checker

---
+++ anagram.py
@@ -0,0 +1,11 @@
+str1 = input ("Enter string1: ")
+str2 = input ("Enter string2: ")
+if (len(str1) != len(str2)):
+	print ("Strings are not anagram")
+else:
+	for i in range(len(str1)):
+		if str1[i] not in str2:
+			print ("String is not an anagram")
+			break
+		elif (i == len(str1)-1):
+			print ("String is an anagram")
